61acabcb8440c2fbae04cdd8a62f923f1ea8ea79
Yunus YILMAZ <yy3474@yandex.com>
2021-01-21 22:12:23 +0300

    update

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Container } from "reactstrap";
 import Navi from "../navi/Navi";
-import Yeni from "../Yeni";
 import Dashboard from "./Dashboard";
 function App() {
   return (
     <Container>
       <Navi />
-      <Yeni />
 
       <Dashboard />
     </Container>
